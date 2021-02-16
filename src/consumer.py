f8694d5d791888b4cfbd21bd91444ce4595fb58e
everton42 <evertonferreira4596@gmail.com>
2021-02-16 19:39:12 -0300

    rabbitmq

---
+++ src/consumer.py
@@ -0,0 +1,20 @@
+import pika
+
+params = pika.URLParameters('amqps://qxbckujb:TNhb9rDz6qr1H3ZyiPn952zP-FM5zjW8@jackal.rmq.cloudamqp.com/qxbckujb')
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='main')
+
+
+def callback(channel, method, properties, body):
+    print('Received in admin')
+    print(body)
+
+
+channel.basic_consume(queue='main', on_message_callback=callback)
+
+print('started Consuming')
+
+channel.start_consuming()
+
+channel.close()
