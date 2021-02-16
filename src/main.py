a5bde9299505bfbeae98b3d2273d2f020346b42f
everton42 <evertonferreira4596@gmail.com>
2021-02-16 14:01:29 -0300

    main modelo

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+CORS(app)
+
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class BookUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    book_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'book_id', name='user_book_unique')
 
 
 @app.route('/')
