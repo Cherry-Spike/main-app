6bf2798a0c90c0f4fee787b7095497ea4b70c6b4
everton42 <evertonferreira4596@gmail.com>
2021-02-16 20:24:02 -0300

    consuming queue

--- src/consumer.py
+++ src/consumer.py
@@ -1,4 +1,8 @@
+import json
+
 import pika
+
+from main import Book, db
 
 params = pika.URLParameters('amqps://qxbckujb:TNhb9rDz6qr1H3ZyiPn952zP-FM5zjW8@jackal.rmq.cloudamqp.com/qxbckujb')
 connection = pika.BlockingConnection(params)
@@ -7,8 +11,28 @@ channel.queue_declare(queue='main')
 
 
 def callback(channel, method, properties, body):
-    print('Received in admin')
-    print(body)
+    print('Received in main')
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'book_created':
+        book = Book(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(book)
+        db.session.commit()
+        print('book_created')
+
+    elif properties.content_type == 'book_updated':
+        book = Book.query.get(data['id'])
+        book.title = data['title']
+        book.image = data['image']
+        db.session.commit()
+        print('book_updated')
+
+    elif properties.content_type == 'book_deleted':
+        book = Book.query.get(data)
+        db.session.delete(book)
+        db.session.commit()
+        print('book_deleted')
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
